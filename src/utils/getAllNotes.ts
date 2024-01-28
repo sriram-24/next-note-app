25b4bbee4ca3a737f871d75ede511cf5eed520bb
sriram-24 <sriram1999m@gmail.com>
2024-01-28 18:03:44 +0530

    api endpoint changes.

--- src/utils/getAllNotes.ts
+++ src/utils/getAllNotes.ts
@@ -1,6 +1,6 @@
 
 export const getAllNotes = async() => {
-    let notesList  = await fetch("http://localhost:3000/api/notes/",{
+    let notesList  = await fetch("/api/notes/",{
         method:"GET",
       })
     let noteListData = await notesList.json()
