46c8aec600fb5f371ce9a615343d1d401279c8bb
sriram-24 <sriram1999m@gmail.com>
2024-02-10 18:37:56 +0530

    Removed unused imports.

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,14 +1,10 @@
 "use client";
-import { INote } from '@/utils/notes';
 import EmptyNoteView from '@/components/EmptyNoteView';
-import Modal from '@/components/Modal'
-import NewNote from '@/components/NewNote'
 import NoteView from '@/components/NoteView';
 import NotesList from '@/components/NotesList';
 import { activeNote } from '@/store/store';
 import { useAtom } from 'jotai';
-import React, { useState } from 'react'
-import {isEmpty} from 'lodash';
+import { isEmpty } from 'lodash';
 
 function HomePage() {
     const [note,setNote] = useAtom(activeNote)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
 "use client"
-import LeftNavigation from '@/components/LeftNavigation'
-import Modal from '@/components/Modal'
-import NewNote from '@/components/NewNote'
-import Image from 'next/image'
-import Link from 'next/link'
-import { useState } from 'react'
-import HomePage from './home/page'
-import { Provider } from 'jotai'
 import { notesStore } from '@/store/store'
+import { Provider } from 'jotai'
+import HomePage from './home/page'
 
 export default function Home() {
 
